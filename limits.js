05ae00e8327e198341b6fc1db23933f958d79878
cjcaf1995 <85407165+cjcaf1995@users.noreply.github.com>
2022-07-16 14:02:33 +0900

    Update limits.js

--- limits.js
+++ limits.js
@@ -1,10 +1,10 @@
 {
   "1sMin": 2,
-  "1sMax": 430,
+  "1sMax": 460,
   "2sMin": 2,
-  "2sMax": 61,
+  "2sMax": 60,
   "3sMin": 2,
-  "3sMax": 76,
+  "3sMax": 83,
   "4sMin": 2,
   "4sMax": 52,
   "5sMin": 2,
